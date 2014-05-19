aecd8fa3676baf6ae9b64443f68c1b348afcd702
yuanying <yuanying@fraction.jp>
2014-05-19 18:02:11 +0900

    My Change.

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "Comments" do
   describe "GET /comments" do
     it "works! (now write some real specs)" do
-      pending
       get comments_path
     end
   end
--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -7,10 +7,6 @@ describe CommentsController do
       { :get => "/comments" }.should route_to(:controller => "comments", :action => "index")
     end
 
-    it "recognizes and generates #recent" do
-      { :get => "/comments/recent" }.should route_to(:controller => "comments", :action => "recent")
-    end
-
     it "recognizes and generates #new" do
       { :get => "/comments/new" }.should route_to(:controller => "comments", :action => "new")
     end
@@ -19,20 +15,20 @@ describe CommentsController do
       { :get => "/comments/1" }.should route_to(:controller => "comments", :action => "show", :id => "1")
     end
 
-    it "should not recognizes and generates #edit" do
-      { :get => "/comments/1/edit" }.should_not be_routable
+    it "recognizes and generates #edit" do
+      { :get => "/comments/1/edit" }.should route_to(:controller => "comments", :action => "edit", :id => "1")
     end
 
     it "recognizes and generates #create" do
       { :post => "/comments" }.should route_to(:controller => "comments", :action => "create")
     end
 
-    it "should not recognizes and generates #update" do
-      { :put => "/comments/1" }.should_not be_routable
+    it "recognizes and generates #update" do
+      { :put => "/comments/1" }.should route_to(:controller => "comments", :action => "update", :id => "1")
     end
 
-    it "should not recognizes and generates #destroy" do
-      { :delete => "/comments/1" }.should_not be_routable
+    it "recognizes and generates #destroy" do
+      { :delete => "/comments/1" }.should route_to(:controller => "comments", :action => "destroy", :id => "1")
     end
 
   end
